f6ed85b5be0be9fc84639ad215ca5c0d338c1f1a
Bineeth Reddy Narra <bineethreddynarra@Bineeths-MacBook-Air.local>
2025-05-28 01:57:54 -0400

    Tested Gradio Sample

--- multimodal.py
+++ multimodal.py
@@ -1,1 +1,8 @@
-print('Hello')
+import gradio as gr
+
+def greet(name):
+    return f"Hello, {name}!"
+
+demo = gr.Interface(fn=greet, inputs="text", outputs="text", title="Greet App")
+
+demo.launch(share=True)
